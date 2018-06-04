45ae960cc78c5679b4fe9726f98c2aa875f7e747
unwriter <alejandra.alejandro@protonmail.com>
2018-06-03 21:59:08 -0400

    get rid of topic post
    too chatty

--- app.js
+++ app.js
@@ -18,43 +18,19 @@ chainfeed.listen(function(res) {
   res.forEach(function(item) {
     if (item.data) {
       let opcode = Buffer.from(item.data[0].buf.data).toString('hex').toLowerCase()
-      if (opcode === '6d02') {
-        // memo post
+      if (opcode === '6d02') { // memo post
         if (item.data.length > 1) {
           let msg = Buffer.from(item.data[1].buf.data).toString('utf-8');
           msg = msg.replace(/@/g, "$")
           msg += (" https://memo.cash/post/" + item.tx.hash)
           send(msg)
         }
-      } else if (opcode === '6d0c') {
-        // memo topic post
-        if (item.data.length > 2) {
-          let hashtag = Buffer.from(item.data[1].buf.data).toString('utf-8');
-          hashtag = hashtag.replace(/@/g, "$")
-          hashtag = hashtag.replace(/\s/g, '')
-          hashtag = "#" + hashtag 
-          let msg = Buffer.from(item.data[2].buf.data).toString('utf-8');
-          msg += (" https://memo.cash/post/" + item.tx.hash)
-          send(msg + " " + hashtag)
-        }
-      } else if(opcode === '8d02') {
-        // blockpress post
+      } else if (opcode === '8d02') { // blockpress post
         if (item.data.length > 1) {
           let msg = Buffer.from(item.data[1].buf.data).toString('utf-8');
           msg = msg.replace(/@/g, "$")
           msg += (" https://www.blockpress.com/posts/" + item.tx.hash)
           send(msg)
-        }
-      } else if (opcode === '8d11') {
-        // blockpress topic post
-        if (item.data.length > 2) {
-          let hashtag = Buffer.from(item.data[1].buf.data).toString('utf-8');
-          hashtag = hashtag.replace(/@/g, "$")
-          hashtag = hashtag.replace(/\s/g, '')
-          hashtag = "#" + hashtag 
-          let msg = Buffer.from(item.data[2].buf.data).toString('utf-8');
-          msg += (" https://www.blockpress.com/posts/" + item.tx.hash)
-          send(msg + " " + hashtag)
         }
       }
     }
